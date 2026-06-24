406422e42a6713fac7d2ffa5dd62437fc81c7bb1
bhavani9848ram <bhavani9848ram@gmail.com>
2026-06-24 13:11:17 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,18 @@
+num1=float(input("Enter first number"))
+num2=float(input("Enter second number"))
+operation=input("Enter +,-,*,/:" )
+if operation=="+":
+    print("Result:",num1+num2)
+elif operation=="-":
+    print("Result:",num1-num2)
+elif operation=="*":
+    print("Result:",num1*num2)
+elif operation=="/":
+    print("Result:",num1/num2)
+    if num2!= 0:
+        print("Result:",num1/num2)  
+    else:
+        print("cannot divide by zero")
+else:
+    print("invalid operation")
+    
